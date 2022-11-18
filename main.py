82c6bb9aeb25ebcd013b767a4b9d536a096bc5b9
AlexSinclair-4 <bokuwakaitodesu@gmail.com>
2022-11-18 10:20:13 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+name = input("Enter Username: ")
+name
+
+age = input("Enter Age: ")
+age
+
+first = int(input("Enter the First Number: "))
+second = int(input("Enter the Second Number: "))
+
+
+print("The username is", name, "and the age is", age)
+print("The product is", first * second)
